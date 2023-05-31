18aa6cae5aaf9a18dc644cfe6c9b31d40d211bd9
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-05-31 21:11:52 +0900

    리액트 라우터, 리코일, 리코일 퍼시스트, classnames, immer 설치

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import {AppBar, Toolbar} from "@mui/material";
+import { Navigate, Route, Routes } from "react-router-dom";
 
+function MainPage() {
+  return (
+    <>
+      <h1>메인 페이지</h1>
+    </>
+  );
+}
+
+function Sub1Page() {
+  return (
+    <>
+      <h1>서브 페이지</h1>
+    </>
+  );
+}
 function App() {
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <div className="flex-1"></div>
-          <div className="font-bold">MY NOTE</div>
+          <div className="font-bold">YEON JI NOTE</div>
           <div className="flex-1"></div>
         </Toolbar>
       </AppBar>
+
+      <Routes>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/sub1" element={<Sub1Page />} />
+        <Route path="/*" element={<Navigate to="/main" />} />
+      </Routes>
     </>
   );
 }
