6f9a94d7ec50921858fc1b8f9b7b0a395eda2fdb
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-06-02 19:43:30 +0900

    할 일 작성 페이지 Form 체크

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,14 +1,51 @@
-import { TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
 
 export default function WritePage() {
-    return (
-      <>
-        <div className= "flex-1 flex p-10 flex-col gap-7 ">
-          <TextField label = "언제 해야하나요?" focused type= "datetime-local"/>
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-          <TextField label="언제 해야하요?" className="flex-1 flex" multiline/>
+    const form = e.target;
 
-        </div>
-      </>
-    );
-  }
+    if (form.regDate.value.length == 0) {
+      alert("날짜를 입력해주세요.");
+      form.regDate.focus();
+      return;
+    }
+
+    if (form.content.value.length == 0) {
+      alert("내용을 입력해주세요.");
+      form.content.focus();
+      return;
+    }
+  };
+
+  return (
+    <>
+      <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit}>
+        <TextField
+          label="언제 해야 하나요?"
+          focused
+          type="datetime-local"
+          name="regDate"
+        />
+
+        <TextField
+          name="content"
+          label="무엇을 해야 하나요?"
+          className="flex-1 flex"
+          InputProps={{ className: "flex-1 flex-col" }}
+          inputProps={{ className: "flex-1" }}
+          multiline
+        />
+
+        <Button type="submit" variant="contained">
+          <span>
+            <i class="fa-solid fa-pencil"></i>
+          </span>
+          <span>&nbsp;</span>
+          <span>할 일 추가</span>
+        </Button>
+      </form>
+    </>
+  );
+}
